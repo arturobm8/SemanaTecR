728e7e10867f5ff7ebf6446fefa3cdc3836a4dd0
Arturo BM <a01168331@tec.mx>
2024-05-06 14:23:47 -0600

    Actividad 2
    Código y doc de la actividad 2

---
+++ retoAnalitica/Codigo/Actividad2.R
@@ -0,0 +1,48 @@
+
+# Problema 1 --------------------------------------------------------------
+
+numero1<-10
+numero2<-3
+total<-numero1+numero2
+cat("La suma de ",numero1," y de ",numero2," es de: ",total)
+
+# Problema 2 --------------------------------------------------------------
+
+lado1<-10
+lado2<-10
+areaCuadrado<-lado1*lado2
+cat("El área de un cuadrado con dos lados de ",lado1," y ",lado2," es de: ",areaCuadrado)
+
+# Problema 3 --------------------------------------------------------------
+
+base<-5.2
+altura<-8.4
+areatriangulo<-(base*altura)/2
+cat("El área del triángulo con base de ",base," y altura de ",altura," es de: ",areatriangulo)
+
+# Problema 4 --------------------------------------------------------------
+
+radio<-7.2
+pi<-3.1416
+areacirculo<-pi*radio^2
+cat("El área del círculo con radio de ",radio," es de: ",areacirculo)
+
+# Problema 5 --------------------------------------------------------------
+
+radio<-34
+altura<-24
+volumen<-(1/3)*pi*(radio^2)*altura
+cat("El volumen del cono recto con radio de ",radio," y altura de ",altura," es de:", volumen)
+
+# Problema 6 --------------------------------------------------------------
+
+pesos<-1000  
+tipo_cambio<-20  
+dolares<-pesos/tipo_cambio
+cat("La cantidad de", pesos, "pesos mexicanos equivale a", dolares, "dólares.")
+
+# Problema 7 --------------------------------------------------------------
+
+gradosF<-50
+gradosC<-(5/9)*(gradosF-32)
+cat(gradosF, "grados Farenheit equivalen a", gradosC, " grados Celsius")
